c4379e52b335eda36f6fc65f4b7a8a26feac76e7
Heber Jimenez <heberedu_17@hotmail.com>
2023-04-10 14:23:15 -0600

    Se agrego la validacion de campos vacios en el formulario

--- DesarrolladorIOSHeberJimene/DesarrolladorIOSHeberJimenez/Views/UserAddViewController.swift
+++ DesarrolladorIOSHeberJimene/DesarrolladorIOSHeberJimenez/Views/UserAddViewController.swift
@@ -44,6 +44,63 @@ class UserAddViewController: UIViewController {
             return
         }
         
+        // - Validacion de campos vacios
+        
+        if nombre == "" {
+            
+            let alert = UIAlertController(title: "No se pudo guardar", message: "El nombre es requerido", preferredStyle: .alert)
+            alert.addAction (
+                UIAlertAction(title: "Aceptar", style: .default)
+                )
+            
+            self.present(alert, animated: true)
+            return
+        }
+        
+        if apellidoPaterno == "" {
+            
+            let alert = UIAlertController(title: "No se pudo guardar", message: "El apellido paterno es requerido", preferredStyle: .alert)
+            alert.addAction (
+                UIAlertAction(title: "Aceptar", style: .default)
+                )
+            
+            self.present(alert, animated: true)
+            return
+        }
+        
+        if apellidoMaterno == "" {
+            
+            let alert = UIAlertController(title: "No se pudo guardar", message: "El apellido materno es requerido", preferredStyle: .alert)
+            alert.addAction (
+                UIAlertAction(title: "Aceptar", style: .default)
+                )
+            
+            self.present(alert, animated: true)
+            return
+        }
+        
+        if correo == "" {
+            
+            let alert = UIAlertController(title: "No se pudo guardar", message: "El correo es requerido", preferredStyle: .alert)
+            alert.addAction (
+                UIAlertAction(title: "Aceptar", style: .default)
+                )
+            
+            self.present(alert, animated: true)
+            return
+        }
+        
+        if telefono == "" {
+            
+            let alert = UIAlertController(title: "No se pudo guardar", message: "El telefono es requerido", preferredStyle: .alert)
+            alert.addAction (
+                UIAlertAction(title: "Aceptar", style: .default)
+                )
+            
+            self.present(alert, animated: true)
+            return
+        }
+        
         UserController.shared.addUser(nombre: nombre, apellidoPaterno: apellidoPaterno, apellidoMaterno: apellidoMaterno, correo: correo, telefono: telefono)
     }
 
